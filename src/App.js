ad6452881532f599eedbda32f20210ca0dfacfa8
Rohit <rkp7133@gmail.com>
2023-08-19 00:31:09 +0530

    adding loader to it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import { useState } from "react";
 import "./App.css";
-import Box from "./Component/Box";
+import Loader from "./Component/Loader/Loader"
 import Container from "./Component/Container";
 import Navigation from "./Component/Navigation";
 
 function App() {
   const [users, setUsers] = useState([]);
-  const [nextSlate, setNextSlate] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const getUsers = async () => {
-    setNextSlate(true);
+    setLoading(true);
     let response = await fetch("https://reqres.in/api/users?page=1");
     const json_response = await response.json();
     setUsers(json_response.data);
+    setLoading(false);
   };
+  
   return (
     <>
-      <Navigation toggle={getUsers} />
-      {nextSlate ? <Container user={users} /> : <Box />}
+      <Navigation  toggle={getUsers} />
+      {!loading && <Container user={users} /> }
+      {loading && <Loader /> }
     </>
   );
 }
---
+++ src/Component/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Loader.module.css";
+
+function Loader() {
+  return (
+    <div className="modal">
+      <div className="loader">scascascas</div>
+    </div>
+  );
+}
+
+export default Loader;
