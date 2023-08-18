b03b3326e11657eb41e6f5367d3f6a63915045c0
Rohit <rkp7133@gmail.com>
2023-08-18 18:56:07 +0530

    started coding and adding Navigation bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navigation from './Component/Navigation';
 
 function App() {
   return (
     <>
-      
+      <Navigation />
     </>
   );
 }
---
+++ src/Component/Navigation.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navigation = () => {
+  return (
+    <>
+      <nav>
+        <div><h1>Logo</h1></div>
+        <ul>
+            <li>HOME</li>
+            <li>ABOUT</li>
+            <li>CONTACT</li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default Navigation;
